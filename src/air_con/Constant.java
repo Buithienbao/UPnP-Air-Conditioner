3e9aef0ba56e67329f225d7c73b3a956122ccd03
dungdinhanh <dinhanhdung1996@gmail.com>
2018-10-06 21:13:25 +0700

    save change

---
+++ src/air_con/Constant.java
@@ -0,0 +1,52 @@
+package air_con;
+
+public class Constant {
+
+    //device
+    public static final String DEVICE_NAME = "AirconditionalSystem";
+    public static final String MANUFACTURER_DETAILS = "2018";
+    public static final String MODEL_DETAILS = "TOSHIBA2018";
+    public static final String MODEL_DESCRIPTION = "Air-Conditional System";
+    public static final String MODEL_NUMBER = "21111996";
+
+    //switchpower
+    public static final String SWITCH_POWER = "SwitchPower";
+    public static final String STATUS = "Status";
+    public static final String GET_STATUS = "GetStatus";
+    public static final String TARGET = "Target";
+    public static final String SET_TARGET = "SetTarget";
+    public static final String GET_TARGET = "GetTarget";
+    public static final String NEW_TARGET_VALUE = "NewTargetValue";
+    public static final String RET_TARGET_VALUe = "RetTargetValue";
+    public static final String RESULT_STATUS = "ResultStatus";
+
+
+    //temprature control service
+    public static final String TEMP_CONTROL = "TempControl";
+    public static final String TEMPERATURE = "Temperature";
+    public static final String GET_TEMPERATURE = "GetTemperature";
+    public static final String INCREASE_TEMPERATURE = "IncreaseTemperature";
+    public static final String DECREASE_TEMPERATURE = "DecreaseTemperature";
+
+
+    //Fan control service
+    public static final String FAN_CONTROL = "FanControl";
+    public static final String FAN_STRENGTH = "FanStrength";
+    public static final String GET_FAN_STRENGTH = "GetFanStrength";
+    public static final String SET_FAN_STRENGTH = "SetFanStrength";
+    public static final String INCREASE_FAN = "IncreaseFan";
+    public static final String DECREASE_FAN = "DecreaseFan";
+    public static final String CHANGE_FAN_DIRECTION = "ChangeFanDirection";
+    public static final String UP_FAN_DIRECTION = "UpFanDirection";
+    public static final String DOWN_FAN_DIRECTION = "DownFanDirection";
+
+    public static final int DEFAULT_FAN_STRENGTH = 5;
+    public static final int MAX_FAN_STRENGTH = 5;
+    public static final int MIN_FAN_STRENGTH = 0;
+    public static final int FAN_STRENGTH_CHANGE = 1;
+    public static final int DEFAULT_FAN_DIRECTION = 3;
+    public static final int MAX_FAN_DIRECTION = 5;
+    public static final int MIN_FAN_DIRECTION = 1;
+    public static final int FAN_DIRECTION_CHANGE = 1;
+
+}
--- src/air_con/Controller.java
+++ src/air_con/Controller.java
@@ -1,4 +1,34 @@
 package air_con;
 
-public class Controller {
+
+import org.fourthline.cling.UpnpService;
+import org.fourthline.cling.model.action.ActionExecutor;
+import org.fourthline.cling.model.meta.Device;
+import org.fourthline.cling.model.meta.RemoteDevice;
+import org.fourthline.cling.registry.DefaultRegistryListener;
+import org.fourthline.cling.registry.Registry;
+import org.fourthline.cling.registry.RegistryListener;
+
+import java.util.concurrent.ScheduledFuture;
+
+
+public class Controller{
+
+    private View view;
+    private Device device;
+    private UpnpService upnpService;
+    private ActionExecutor actionExecutor;
+    private ScheduledFuture scheduledFuture;
+    private RegistryListener registryListener = new DefaultRegistryListener();
+
+
+    public void remoteDeviceAdded(Registry registry, RemoteDevice remoteDevice)
+    {
+        System.out.println("Group4 remote device detected.");
+        if(remoteDevice.getDetails().getModelDetails().getModelName().equals(Constant.MODEL_DETAILS))
+        {
+            System.out.println(""+Constant.MODEL_DESCRIPTION+ " is detected");
+            device = remoteDevice;
+        }
+    }
 }
